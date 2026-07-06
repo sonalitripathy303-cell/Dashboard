135d57f68341d4dcc463bafdaf55a013d364b4aa
sonalitripathy303-cell <sonalitripathy303@gmail.com>
2026-07-06 16:48:52 +0530

    add inventory

---
+++ Models/ReportRowModel.cs
@@ -0,0 +1,12 @@
+﻿namespace LoginToDashboard.Models
+{
+    public class ReportRowModel
+    {
+        public string ItemName { get; set; }
+        public DateTime? SoldDate { get; set; }
+        public int SoldQuantity { get; set; }
+        public decimal SoldPrice { get; set; }
+        public int RemainingQuantity { get; set; }
+        public decimal RemainingPrice { get; set; }
+    }
+}
